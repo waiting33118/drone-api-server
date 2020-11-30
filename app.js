e1ba8aa611cfd71b8f2b08e2e04a67cd36e0641b
Tony <waiting33118@gmail.com>
2020-11-30 15:39:36 +0800

    feat: create server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const { useMqtt } = require('./configs/mqtt')
+// const socketIO = require('socket.io')
+const routes = require('./routes')
+const app = express()
+const PORT = 3030
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+useMqtt()
+
+app.use('/', routes)
+app.listen(PORT, () => console.log(`The server is listening on port ${PORT.toString()}`))
