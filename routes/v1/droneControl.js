00db21130b89d653b5a9aa69f3baa5068a8d86fa
Tony <waiting33118@gmail.com>
2020-12-02 18:11:21 +0800

    feat: add routes (arm/disarm,change flight height)

--- routes/v1/droneControl.js
+++ routes/v1/droneControl.js
@@ -2,10 +2,12 @@ const express = require('express')
 const droneAPI = require('../../controllers/drone')
 const router = express.Router()
 
+router.post('/arm', droneAPI.arm)
+router.post('/disarm', droneAPI.disArm)
 router.post('/takeoff', droneAPI.takeOff)
 router.post('/land', droneAPI.land)
 router.post('/goto', droneAPI.goTo)
 router.post('/cfa', droneAPI.changeFlightAltitude)
-router.post('/rtl', droneAPI.returnToLand)
+router.post('/cfm', droneAPI.changeFlightMode)
 
 module.exports = router
