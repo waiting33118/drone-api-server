5be4fd9b0d2a64378344871cf023cdb565e8055f
Tony Chung <waiting33118@gmail.com>
2020-12-26 21:21:07 +0800

    feat: add changeyaw API & edit API name

--- routes/v1/droneControl.js
+++ routes/v1/droneControl.js
@@ -3,13 +3,13 @@ const droneAPI = require('../../controllers/drone')
 const router = express.Router()
 
 router.post('/arm', droneAPI.arm)
-router.post('/disarm', droneAPI.disArm)
+router.post('/disarm', droneAPI.disarm)
 router.post('/takeoff', droneAPI.takeOff)
 router.post('/land', droneAPI.land)
 router.post('/goto', droneAPI.goTo)
-router.post('/cfa', droneAPI.changeFlightAltitude)
-router.post('/cfm', droneAPI.changeFlightMode)
-router.post('/cas', droneAPI.changeAirSpeed)
-router.post('/ssc', droneAPI.servoContol)
+router.post('/changeflightmode', droneAPI.changeFlightMode)
+router.post('/changespeed', droneAPI.changeSpeed)
+router.post('/changeyaw', droneAPI.changeYaw)
+router.post('/servocontrol', droneAPI.servoContol)
 
 module.exports = router
