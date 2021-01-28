d4366eadd77bcba09723d8840188cc80d289a01b
Tony Chung <waiting33118@gmail.com>
2021-01-28 16:23:02 +0800

    feat: edit test script

--- test/mosquitto.sh
+++ test/mosquitto.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mosquitto_sub -h 35.201.182.150 -t drone/cmd -v
+# mosquitto_sub -h 35.201.182.150 -t drone/message -v&\
+mosquitto_sub -h 35.201.182.150 -t drone/cmd_ack -v&\
+mosquitto_sub -h 35.201.182.150 -t drone/mission_ack -v&
+mosquitto_sub -h 35.201.182.150 -t drone/apm_text -v
+
