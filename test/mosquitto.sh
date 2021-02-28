62b8df2e4548c9f3fa9e4c59f63ac7426bf0d298
Tony Chung <waiting33118@gmail.com>
2021-02-28 20:13:20 +0800

    feat: remove modquitto script

--- test/mosquitto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# mosquitto_sub -h 35.201.182.150 -t drone/message -v&\
-mosquitto_sub -h 35.201.182.150 -t drone/cmd_ack -v&\
-mosquitto_sub -h 35.201.182.150 -t drone/mission_ack -v&
-mosquitto_sub -h 35.201.182.150 -t drone/apm_text -v
-
