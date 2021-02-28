636b8df82ea5a083aaffd46575109a0f2674dbfb
Tony Chung <waiting33118@gmail.com>
2021-02-28 22:36:47 +0800

    feat: edit output module of routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const swaggerUI = require('swagger-ui-express')
 const router = express.Router()
 const apiV1 = require('./v1/api')
-const { spec } = require('../config/swaggerJsdoc')
+const { spec } = require('../config')
 
 router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(spec, { explorer: true }))
 router.use('/api/v1', apiV1)
 
-module.exports = router
+module.exports.routes = router
