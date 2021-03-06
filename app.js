6207203c5bffe3c6c72fd1960afb93f19e3da1e3
Tony Chung <waiting33118@gmail.com>
2021-03-06 20:30:10 +0800

    feat: edit app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const { routes } = require('./routes')
 const { PORT } = process.env
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(routes)
 
@@ -22,7 +22,7 @@ mqttInit(io)
 
 server.listen(PORT, async () => {
   console.log(`The server is listening on port ${PORT}`)
-  console.log('API docs: http://127.0.0.1:3030/api-docs')
+  console.log('API docs: http://0.0.0.0:3030/api-docs')
   try {
     await db.authenticate()
     console.log('Database connection has been established successfully.')
