57ff3b38649d40dfae9e05b6a19ec95e2c6504bc
Tony Chung <waiting33118@gmail.com>
2021-03-06 20:34:35 +0800

    feat: edit default service router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const swaggerUI = require('swagger-ui-express')
 const router = express.Router()
 const apiV1 = require('./v1/api')
 const { spec } = require('../config')
+const { checkStatus } = require('../controllers')
 
+router.get('/', checkStatus)
 router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(spec, { explorer: true }))
 router.use('/api/v1', apiV1)
 
