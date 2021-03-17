8fc6828b43eb9627abf2a9a83f496ab765a305ad
Tony Chung <waiting33118@gmail.com>
2021-03-17 19:33:15 +0800

    feat: create mqtt sub/unsubscribe mechanism

--- config/mqtt.js
+++ config/mqtt.js
@@ -1,65 +1,106 @@
 const mqtt = require('mqtt')
-const client = mqtt.connect('tcp://35.201.182.150')
+const { MQTT_USERNAME, MQTT_PASSWORD, MQTT_HOST } = process.env
+const test = mqtt.connect(MQTT_HOST, {
+  username: MQTT_USERNAME,
+  password: MQTT_PASSWORD,
+  port: 1883
+})
+
+// Test MQTT connection
+test.on('connect', () => {
+  console.log('MQTT connection test succeed!')
+  test.end()
+})
+test.on('error', (error) => console.log(error))
+
+const mqttInit = (io) => {
+  io.on('connection', socket => {
+    // Create MQTT connection when socket established
+    const subscribeClient = mqtt.connect(MQTT_HOST, {
+      username: MQTT_USERNAME,
+      password: MQTT_PASSWORD,
+      port: 1883
+    })
+
+    // New MQTT connection message
+    subscribeClient.on('connect', () => console.log('New mqtt connection established', 'clientId:', subscribeClient.options.clientId))
+
+    // New socket connection message
+    console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }), 'socketId:', socket.id)
+
+    // disconnect message
+    socket.on('disconnect', (reason) => {
+      console.log(new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' }), reason)
+      subscribeClient.end()
+    })
+
+    // Get drone ID and subscribe mqtt topics
+    socket.on('mqttSubscribe', (user) => {
+      console.log(`\nUser: [${user.name}] ID: [${user.droneId}] connected! Start subscribing topics`)
+      // subscribe
+      subscribeClient.subscribe(topicGenerator(user.droneId), (err, success) => {
+        if (!err) {
+          console.log(success)
+          return
+        }
+        console.log(err)
+      })
+    })
+
+    socket.on('mqttUnsubscribe', (droneId) => {
+      // unsubscribe
+      subscribeClient.unsubscribe(topicGenerator(droneId), (err) => {
+        if (!err) {
+          console.log(`${droneId} Unsubscribe all topics`)
+          subscribeClient.end()
+          return
+        }
+        console.log(err)
+      })
+    })
+
+    // listen mqtt message and send it to frontend clients
+    subscribeClient.on('message', (topic, payload) => {
+      switch (topic.slice(topic.indexOf('/') + 1)) {
+        case 'message':
+          socket.emit(topic, parseMessage(payload))
+          break
+        case 'cmd_ack':
+          socket.emit(topic, parseMessage(payload))
+          break
+        case 'mission_ack':
+          socket.emit(topic, parseMessage(payload))
+          break
+        case 'apm_text':
+          socket.emit(topic, parseMessage(payload))
+          break
+      }
+    })
+  })
+}
 
 /**
  * Turn buffer message to JSON format
  * @param {Buffer} message the message sent from mqtt broker
  * @returns {object} formated message
  */
-const parseMessage = (message) => {
+function parseMessage (message) {
   const formattedMsg = JSON.parse(message.toString())
   return formattedMsg
 }
 
-const mqttInit = (io) => {
-  io.on('connection', socket => {
-    console.log(new Date().toLocaleString(), socket.id)
-    socket.on('disconnect', (reason) => console.log(new Date().toLocaleString(), reason))
-  })
-
-  client.on('connect', () => {
-    client.subscribe('drone/message', (err, success) => {
-      if (err) return console.log(err)
-      console.log('Subscribe Topic: drone/message')
-    })
-    client.subscribe('drone/cmd_ack', (err, success) => {
-      if (err) return console.log(err)
-      console.log('Subscribe Topic: drone/cmd_ack')
-    })
-    client.subscribe('drone/mission_ack', (err, success) => {
-      if (err) return console.log(err)
-      console.log('Subscribe Topic: drone/mission_ack')
-    })
-    client.subscribe('drone/apm_text', (err, success) => {
-      if (err) return console.log(err)
-      console.log('Subscribe Topic: drone/apm_text')
-    })
-  })
-
-  client.on('message', (topic, message) => {
-    switch (topic) {
-      case 'drone/message': {
-        const msg = parseMessage(message)
-        io.emit('message', msg)
-        break
-      }
-      case 'drone/cmd_ack': {
-        const msg = parseMessage(message)
-        io.emit('ack', msg)
-        break
-      }
-      case 'drone/mission_ack': {
-        const msg = parseMessage(message)
-        io.emit('mission', msg)
-        break
-      }
-      case 'drone/apm_text': {
-        const msg = parseMessage(message)
-        io.emit('apm', msg)
-        break
-      }
-    }
-  })
+/**
+ * Return a topic array that need to subscribe
+ * @param {number} droneId
+ * @returns topic array
+ */
+function topicGenerator (droneId) {
+  return [
+    `${droneId}/message`,
+    `${droneId}/cmd_ack`,
+    `${droneId}/mission_ack`,
+    `${droneId}/apm_text`
+  ]
 }
 
 module.exports = mqttInit
