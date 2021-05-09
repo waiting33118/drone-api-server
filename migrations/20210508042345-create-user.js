0012db4c229657564e26b89a2d0790d3bcb14a2d
Tony Chung <waiting33118@gmail.com>
2021-05-09 21:57:38 +0800

    chore: create migrations

---
+++ migrations/20210508042345-create-user.js
@@ -0,0 +1,40 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: 'Pilot'
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      droneId: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
+  }
+}
