43c81851be3a5988751d86831b90dbedcbf73aeb
Tony Chung <waiting33118@gmail.com>
2021-05-09 22:21:41 +0800

    feat: edit server entry point

--- app.js
+++ app.js
@@ -1,33 +1,48 @@
 const express = require('express')
+const cors = require('cors')
+const http = require('http')
+const cookieParser = require('cookie-parser')
 const app = express()
-const server = require('http').createServer(app)
+const server = http.createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
+    origin: ['http://localhost:8080', 'https://localhost:8080']
   }
 })
-const cors = require('cors')
-const { mqttInit, signalInit, db } = require('./config')
-const { routes } = require('./routes')
+const routes = require('./routes')
+const { db, log, mqttInit, signalInit } = require('./libs')
 
 const { PORT } = process.env
 
+app.use(cors({
+  origin: ['http://localhost:8080', 'https://localhost:8080']
+}))
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
 app.use(routes)
 
 mqttInit(io)
 signalInit(io)
 
 server.listen(PORT, async () => {
-  console.log(`The server is listening on port ${PORT}`)
-  if (process.env.NODE_ENV === 'development') console.log('API docs: http://0.0.0.0:3030/api-docs')
+  log.serverStarted(PORT)
   try {
     await db.authenticate()
-    console.log('Database connection has been established successfully.')
+    log.databaseCheckSuccess()
   } catch (error) {
-    console.log('Unable to connect to the database:', error)
+    log.databaseCheckFailed(error.message)
   }
 })
+
+process.on('SIGINT', () => {
+  log.terminateSignal()
+  process.exit()
+})
+
+process.on('SIGTERM', () => {
+  log.terminateSignal()
+  process.exit()
+})
+
+process.on('exit', (code) => log.processExit(code))
