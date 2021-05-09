1af561b6fbae255be40527454311f24116759f2c
Tony Chung <waiting33118@gmail.com>
2021-05-09 22:13:24 +0800

    feat: export module of middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,40 +1,5 @@
-const jwt = require('jsonwebtoken')
+const authMiddleware = require('./auth')
 
-const checkAuthenticate = (req, res, next) => {
-  const { ACCESS_TOKEN_SECRET } = process.env
-  const { authorization } = req.headers
-
-  if (!authorization) {
-    return res.status(401).json({
-      status: 'error',
-      errCode: 703,
-      msg: 'Access token missing!'
-    })
-  }
-
-  const token = authorization.replace('Bearer ', '')
-
-  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      const { name } = err
-      if (name === 'TokenExpiredError') {
-        return res.status(401).json({
-          status: 'error',
-          errCode: 704,
-          msg: 'Access token Expired!'
-        })
-      }
-      if (name === 'JsonWebTokenError') {
-        return res.status(401).json({
-          status: 'error',
-          errCode: 705,
-          msg: 'Access Token Error!'
-        })
-      }
-    }
-    req.body.userId = decoded.userId
-    next()
-  })
+module.exports = {
+  authMiddleware
 }
-
-module.exports.checkAuthenticate = checkAuthenticate
