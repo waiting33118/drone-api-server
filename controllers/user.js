692640fa62187de7690fe518ed8809334bcf42df
Tony Chung <waiting33118@gmail.com>
2021-05-16 22:10:04 +0800

    feat: user-fetch remove left join token, return user info exclude password

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,17 @@
 const db = require('../models')
-const { User, Token } = db
+const { User } = db
 
 const userService = {
   async fetchUserInfo (req, res) {
     const { userId } = req.body
-    const user = await User.findByPk(userId, { include: [Token] })
-    res.json(user.toJSON())
+    const user = await User.findByPk(userId)
+    const { id, name, email, droneId } = user.toJSON()
+    res.json({
+      id,
+      name,
+      email,
+      droneId
+    })
   }
 
   // TODO: /user/edit
