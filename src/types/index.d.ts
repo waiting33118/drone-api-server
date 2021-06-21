78519277af9e6c96efe9ff148eacdc9bb8a710ba
Tony Chung <waiting33118@gmail.com>
2021-06-21 14:43:15 +0800

    feat: create type definitions

---
+++ src/types/index.d.ts
@@ -0,0 +1,37 @@
+export interface ENV_VARIABLE {
+  HTTP_PORT?: string
+  HTTPS_PORT?: string
+  NODE_ENV?: string
+  RABBITMQ_HOSTNAME?: string
+  RABBITMQ_USERNAME?: string
+  RABBITMQ_PASSWORD?: string
+  RABBITMQ_PORT?: string
+  MYSQL_HOSTNAME?: string
+  MYSQL_PORT?: string
+  MYSQL_USERNAME?: string
+  MYSQL_PASSWORD?: string
+  JWT_TOKEN_SECRET?: string
+  PRIVATE_KEY_PATH?: string
+  CERTIFICATE_PATH?: string
+}
+
+export type SignupField = {
+  email: string
+  password: string
+  checkPassword: string
+  droneId: string
+}
+
+export type LoginField = {
+  email: string
+  password: string
+}
+
+export type TokenPayload = {
+  uuid: string
+}
+
+export type CookiePayload = {
+  access_token: string
+  refresh_token: string
+}
