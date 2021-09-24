307d6154331b257b17596fe36b9d11acf96e1086
Tony Chung <waiting33118@gmail.com>
2021-09-24 13:12:26 +0800

    feat: create get all flight records & specific flight record

--- src/services/drone.ts
+++ src/services/drone.ts
@@ -20,5 +20,33 @@ export default {
       res.status(500).json({ msg: 'Internal server error' });
       logger.error(error);
     }
+  },
+
+  async getFlightRecords(req: Request, res: Response) {
+    const uuid: string = res.locals.uuid;
+
+    try {
+      const flightRepo = getRepository(Flight);
+      const records = await flightRepo.find({
+        where: { user: uuid }
+      });
+      res.json({ records });
+    } catch (error) {
+      res.status(500).json({ msg: 'Internal server error' });
+      logger.error(error);
+    }
+  },
+
+  async getFlightRecord(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const flightRepo = getRepository(Flight);
+      const record = await flightRepo.findOne(id);
+      res.json({ record });
+    } catch (error) {
+      res.status(500).json({ msg: 'Internal server error' });
+      logger.error(error);
+    }
   }
 };
